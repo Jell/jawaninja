b81a86cafe1d943eb74d3e39d3bb39c6e4dd3a2b
Jell <jean-louis@icehouse.se>
2011-10-02 12:45:12 +0200

    Revert "Added extra css for handheld devices."
    This reverts commit c73f89f2abda95adf5df943876a3d75a531571b6.

--- src/jawaninja/views/common.clj
+++ src/jawaninja/views/common.clj
@@ -14,7 +14,6 @@
 
 (def includes {:jquery (include-js "http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js")
                :default (include-css "/css/default.css")
-               :handheld [:link {:type "text/css" :href "/css/handheld.css" :media "handheld" :rel "stylesheet"}]
                :reset (include-css "/css/reset.css")
                :blog.js (include-js "/js/blog.js")})
 
@@ -65,7 +64,7 @@
 
 (defpartial main-layout [& content]
   (html5
-    (build-head [:reset :default :handheld :jquery :blog.js])
+    (build-head [:reset :default :jquery :blog.js])
     [:body
       (github-banner)
      [:div#page
