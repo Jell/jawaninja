f6b86dfbc6e52ca62839e7b146c22989756e0493
Jell <jean-louis@icehouse.se>
2011-10-09 18:25:35 +0200

    Update Clojure and Noir.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject jawaninja "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.2.1"]
+            :dependencies [[org.clojure/clojure "1.3.0"]
                            [clj-time "0.3.0"]
-                           [noir "1.1.0"]
+                           [noir "1.2.0"]
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
                            [org.clojure/java.jdbc "0.0.6"]
                            [mysql/mysql-connector-java "5.1.17"]
