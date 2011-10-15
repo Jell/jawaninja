8a8677925f2d4179e3af1c50019858abbc8ca0f7
Jell <jean-louis@icehouse.se>
2011-10-15 22:42:32 +0200

    Update jdbc.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                            [clj-time "0.3.0"]
                            [noir "1.2.0"]
                            [org.markdownj/markdownj "0.3.0-1.0.2b4"]
-                           [org.clojure/java.jdbc "0.0.6"]
+                           [org.clojure/java.jdbc "0.0.7"]
                            [mysql/mysql-connector-java "5.1.17"]
                            [clj-yaml "0.3.0-SNAPSHOT"]]
             :main jawaninja.server)
