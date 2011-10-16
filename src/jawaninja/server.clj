942ef9b4df20c17f84e59ef5cd1e51f830b2135c
Jell <jean-louis@icehouse.se>
2011-10-16 17:34:08 +0200

    Do not reset database every time.

--- src/jawaninja/server.clj
+++ src/jawaninja/server.clj
@@ -7,7 +7,6 @@
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
-    (models/initialize!)
     (server/start port {:mode mode
                         :ns 'jawaninja})))
 
